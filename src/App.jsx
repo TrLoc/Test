798f3a1e252b5ca58f64b1ea4e23f19a2d68076c
TrLoc <124898432+TrLoc@users.noreply.github.com>
2023-11-14 16:19:24 +0700

    test

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './components/header'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+
+function App() {
+  return (
+      <div>
+        <Header/>
+      </div>
+  )
+}
+
+export default App
